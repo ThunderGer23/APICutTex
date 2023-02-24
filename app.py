41051d028584c87eca7ba75b758f2de9a21a9dc3
ThunderGer <Jaames11@hotmail.com>
2023-02-23 22:52:50 -0600

    Prueba 23-03-23

--- app.py
+++ app.py
@@ -5,28 +5,25 @@ fi = chain.from_iterable
 
 pdfFile = open('document\TesisNotGenitoIndice.pdf', 'rb')
 pdfRead = PyPDF2.PdfFileReader(pdfFile)
+indice = {}
 
 def keepalnum(i):
-    for j in i:
-        if not (j.isalnum()):
-            i.remove(j)
-    return i
+    return (i.isalnum() or i==' ')
 
 def processText(page: str, test: str):
-    page = page.replace('.', ' ').lower().split('\n')
+    page = [''.join(list(filter(keepalnum,p))) for p in list(page.lower().split('\n'))]
+    # page = page.replace('.', ' ').lower().split('\n')
     page = [p.strip() for p in page]
     title = page.index(gcm(test, page)[0])
     page.pop(title)
     page = [list(filter(('').__ne__,p.split(' '))) for p in page]
     # Test = list(filter(keepalnum,page))
-    # print(Test)
-    Test = []
-    for i in page:
-        for j in i:
-            if not(j.isalnum()):
-                j = j[1:]
-            Test.append(j)
-    print(Test)
+    test = {'page':page[0]}
+    for p in range(len(page)):
+        test[f's{p}'] = page[p]
+    test.pop('s0')
+    return test
+    
         
 
 def readFile():
@@ -36,7 +33,14 @@ def readFile():
         compare = page.extract_text().lower()[:20].split(' ')
         test = list(fi([gcm(x,compare) for x in ['indice','contenido']]))
         if(len(test)):
-            processText(page.extract_text(), test[0])
-            break
+            global indice
+            indice = processText(page.extract_text(), test[0])
+            pass
+        if (gcm(x,indice.get('page')) for x in [page.extract_text()]):
+            pass
+        else:
+            print(page.extract_text())
+            print('holi') 
+        #Jalar texto :'v
 
 readFile()
