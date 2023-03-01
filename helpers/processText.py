b432ddd4c6912e7e378fcaf8e11604315bac21d4
ThunderGer <Jaames11@hotmail.com>
2023-03-01 02:08:45 -0600

    Falta traer texto de las secciones, ya se tiene mapeada los indices con cada sección del documento

--- helpers/processText.py
+++ helpers/processText.py
@@ -1,6 +1,5 @@
-def processText(page, test: str):
-    p = list(map(lambda i : i.strip(), [''.join(list(filter(lambda i : (i.isalnum() or i==' '),p))) for p in list(page.lower().split('\n'))]))
-    p.pop([x for x in range(len(p)) if test in p[x]].pop())
+def processText(hojas):
+    p = list(map(lambda i : i.strip(), [''.join(list(filter(lambda i : (i.isalnum() or i==' '),p))) for p in list(hojas.lower().split('\n'))]))
     test = {'page':p[0]}
     for pa in range(len(p)): test[f's{pa}'] = p[pa]
     test.pop('s0')
--- routes/cutFile.py
+++ routes/cutFile.py
@@ -5,16 +5,21 @@ from itertools import chain
 from helpers.processText import processText as pT
 FI = chain.from_iterable
 indice = {}
-
+hojas = []
+control = 0
 cF = APIRouter()
 
 # 'document\TesisNotGenitoIndice.pdf'
 @cF.get('/indice', response_model= dict, tags=['Get indice'])
 def readFile(rute: str):
+    global control
     pdfRead = PyPDF2.PdfFileReader(open(rute, 'rb'))
     for i in range(0, int(pdfRead.getNumPages()/10)):
-        page = pdfRead.getPage(i)
-        test = list(FI([gcm(x,page.extract_text().lower()[:20].split(' ')) for x in ['indice','contenido']]))
-        if (len(test) and ((test[0] == 'índice') or (test[0] == 'contenido'))): indice['Ip'+str(i)] = pT(page.extract_text(), test[0])
+        if(len(list(FI([gcm(x,pdfRead.getPage(i).extract_text().lower()[:20].split(' ')) for x in ['indice','contenido']])))): hojas.append(i)
+
+    for i in range(1, len(hojas)):
+        for hoja in range(hojas[i-1], hojas[i]): indice['Ip'+str(hoja)] = pT(pdfRead.getPage(hoja).extract_text())
+    indice['Ip'+str(hojas[-1])] = pT(pdfRead.getPage(hojas[-1]).extract_text())
     # Aquí se mandan las peticiones a la API de parafraseo, en base al contenido del indice
+    # print(pdfRead.getPage(27).extract_text().lower()[:])
     return indice
