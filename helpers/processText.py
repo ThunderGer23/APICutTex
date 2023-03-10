48f3598f6d014613697f59b0589c7a368ccdaddd
ThunderGer <Jaames11@hotmail.com>
2023-03-10 01:18:41 -0600

    Listo el marco teorico, nomas el requests

--- helpers/processText.py
+++ helpers/processText.py
@@ -1,4 +1,5 @@
 from difflib import get_close_matches as gcm
+import requests
 import PyPDF2
 import re
 pdfRead = PyPDF2.PdfFileReader(open('document\TesisNotGenitoIndice.pdf', 'rb'))
@@ -9,9 +10,8 @@ def processText(hojas):
     for pa in range(len(p)): test[f's{pa}'] = p[pa]
     if (gcm(test['s1']) for x in ['indice','contenido']): test.pop('s1')
     test.pop('s0')
-
     coincidencias = list(set([ match for valor in test.values() for match in gcm((lambda x: x.split()[0] if len(x.split()) > 0 else "")(valor), ['objetivo', 'objetivos', 'marco', 'estado', 'planteamiento', 'justificación', 'análisis', 'funcional'],n = 10, cutoff=0.3) ]))
-    
+
     resultados = [f"{k} {v}" for k, v in test.items() if any(c in v for c in coincidencias)]
     if(len(resultados)): sections(resultados)
     return test
@@ -22,55 +22,25 @@ def delnum(s):
     return s
 
 def sections(section):
-    # print(list(i.split() for i in section))
     s = {}
+    match = ""
     secforana = ""
-    CONT = 0
     for i in section:
         aux = i.split()
         s[aux[0]] = {"sec": " ".join(aux[1:-1]), "pag": aux[-1]}
-
     arr = list(s.keys())
-    for i in range(2, len(arr)):
+    for i in range(len(arr)):
         title = " ".join(delnum(s[arr[i]]['sec']).split()[:-1])
         pretitle = " ".join(delnum(s[arr[i-1]]['sec']).split()[:-1])
         page = int(s[arr[i]]['pag'])
         pageRange = page - int(s[arr[i-1]]['pag'])
         regex = re.compile(pretitle + r' \n*(.*?).\n*'+ title, re.DOTALL)
-
         if (pageRange != 0):
-            print(title)
-            print(pretitle)
-            # caso especialpara que las secciones de 2 hojas las imprima
-            for ran in range(page, page + pageRange):
-                pa = pdfRead.getPage(ran-1).extract_text().lower()
-                secforana += pa
+            for ran in range(page - pageRange, page+1): secforana += pdfRead.getPage(ran-1).extract_text().lower()
             match = regex.search(secforana)
-            if(match != None):
-                sec = match.group(1)
-                print(sec)
-                CONT += 1
-        else:
-            # print(pageRange)
-            # print(title)
-            # print(pretitle)
-            pa = pdfRead.getPage(int(s[arr[i]]['pag'])-1).extract_text().lower()
-            match = regex.search(pa)
-            if(match != None):
-                sec = match.group(1)
-                print(sec)
-                CONT += 1
-        # if(CONT == 5): break
-        # print(pretitle, title)
-        # # No encuentra porque hay que guardar el rando de hojas en donde se va a buscar
-        # # Si ambos están en la misma hoja solo lee una y ya, si son diferentes hace un
-        # # append a un array de las hojas y extrae el texto de todas ellas para después seccionar
-        # page = pdfRead.getPage(int(s[arr[i]]['pag'])-1).extract_text().lower()
-        # regex = re.compile(pretitle+r' \n*(.*?).\n*'+title, re.DOTALL)
-        # match = regex.search(page)
-        # if(match != None):
-        #     sec = match.group(1)
-            # print(sec)
-        # break
-    # return s
+        else: match = regex.search(pdfRead.getPage(int(s[arr[i]]['pag'])-1).extract_text().lower())
+        analysis = match.group(1) if(match != None) else None
+        r = requests.get('https://pathfake:v/{analysis}')
+        if(r.status_code): r.json()
+    return r
 
