ef098a3aebdd6ad523821b60ca7b28a9253b83f9
ThunderGer <Jaames11@hotmail.com>
2023-04-10 23:01:09 -0600

    APICutText ya deploya malditasea!! >:c x4 ya esta listo :'c x3 ahora si :'v

--- routes/cutFile.py
+++ routes/cutFile.py
@@ -13,7 +13,7 @@ cF = APIRouter()
 
 # 'document\TesisNotGenitoIndice.pdf'
 @cF.get('/indice', response_model= dict, tags=['indice'])
-def readFile(name: str):
+def readFile(name):
     global control
     print(exists(f'./document/{name}'))
     pdfRead = PyPDF2.PdfFileReader(open(f'./document/{name}', 'rb'))
@@ -21,7 +21,7 @@ def readFile(name: str):
         if(len(list(FI([gcm(x,pdfRead.getPage(i).extract_text().lower()[:20].split(' ')) for x in ['indice','contenido']])))): hojas.append(i)
 
     for i in range(1, len(hojas)):
-        for hoja in range(hojas[i-1], hojas[i]): indice['Ip'+str(hoja)] = pT(pdfRead.getPage(hoja).extract_text())
+        for hoja in range(hojas[i-1], hojas[i]): indice['Ip'+str(hoja)] = pT(pdfRead.getPage(hoja).extract_text(), name)
     indice['Ip'+str(hojas[-1])] = pT(pdfRead.getPage(hojas[-1]).extract_text(), name)
     return indice
 
